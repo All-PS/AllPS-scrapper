d248de579ee1bc00b705022c8f10999e2c449f7a
Jaeseung Jason Chung <51861361+victorychung6@users.noreply.github.com>
2023-10-29 15:39:44 +0900

    feat: 예외처리

--- app/service/BaekjoonService.py
+++ app/service/BaekjoonService.py
@@ -14,23 +14,28 @@ from app.util.ChromeDriver import ChromeDriver
 from app.util.DatabaseConnection import DatabaseConnection
 from app.util.SlackBot import SlackBot
 
+
 def getPageNumber(driver):
     driver.get('https://www.acmicpc.net/problemset')
     pages_text = driver.find_elements(By.XPATH, '//ul[@class=\'pagination\']/li')[-1].text
     pages = int(pages_text)
     return pages
 
+
 def openProblemSetPage(driver, page):
     driver.get('https://www.acmicpc.net/problemset/' + str(page))
+
 
 def openNewTab(driver, link):
     driver.execute_script("window.open('', '_blank');")
     driver.switch_to.window(driver.window_handles[-1])
     driver.get(link)
 
+
 def closeNewTab(driver):
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
+
 
 def getProblemData(driver, link, wait):
     code = re.search(r'/(\d+)$', driver.current_url).group(1)
@@ -44,6 +49,7 @@ def getProblemData(driver, link, wait):
     problem = BaekjoonProblem(code=code, name=name, url=url, tier=tier, categories=categories, updatedAt=now)
     ProblemDao.save(problem)
 
+
 def crawlProblems():
     driver = ChromeDriver()
     wait = WebDriverWait(driver, 10)
@@ -53,27 +59,38 @@ def crawlProblems():
 
     for page in range(1, pages + 1):
         openProblemSetPage(driver, page)
-
-        wait.until(EC.presence_of_element_located((By.XPATH, '//tbody/tr')))
-        problems = driver.find_elements(By.XPATH, '//tbody/tr')
-
+        problems = getProblems(driver, wait)
         DatabaseConnection.startTransaction()
 
         for idx, problem in enumerate(problems, 1):
-            wait.until(EC.presence_of_element_located((By.XPATH, './/td[2]/a')))
-            link = problem.find_element(By.XPATH, './/td[2]/a').get_attribute('href')
-
+            link = getProblemLink(problem, wait)
             openNewTab(driver, link)
-            getProblemData(driver, link, wait)
-            closeNewTab(driver)
 
-            # 매 10개의 문제마다 트랜잭션 커밋
-            if idx % 10 == 0:
-                DatabaseConnection.commitTransaction()
-                DatabaseConnection.startTransaction()
+            try:
+                getProblemData(driver, link, wait)
+            except Exception as e:
+                print(idx, "번 크롤링 실패\n Exception: ", e)
+            finally:
+                closeNewTab(driver)
+                # 매 10개의 문제마다 트랜잭션 커밋
+                if idx % 10 == 0:
+                    DatabaseConnection.commitTransaction()
+                    DatabaseConnection.startTransaction()
+                time.sleep(random.uniform(8, 12))
 
         # 남은 트랜잭션 커밋
         DatabaseConnection.commitTransaction()
 
-        time.sleep(random.uniform(8, 12))
         SlackBot.alert(f"백준 {page} 페이지의 크롤링이 완료되었습니다.")
+
+
+def getProblemLink(problem, wait):
+    wait.until(EC.presence_of_element_located((By.XPATH, './/td[2]/a')))
+    link = problem.find_element(By.XPATH, './/td[2]/a').get_attribute('href')
+    return link
+
+
+def getProblems(driver, wait):
+    wait.until(EC.presence_of_element_located((By.XPATH, '//tbody/tr')))
+    problems = driver.find_elements(By.XPATH, '//tbody/tr')
+    return problems
