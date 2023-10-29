b00d5736fa9a0ce9b0dcbadde3cad89359d56983
1Bronze <khdkkhdd@gmail.com>
2023-10-29 16:56:35 +0900

    [fix] captcha 우회

--- app/util/ChromeDriver.py
+++ app/util/ChromeDriver.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -12,7 +13,9 @@ class ChromeDriver:
         if not cls._instance:
             # Chrome 디버깅 모드로 실행
             chrome_process = subprocess.Popen(
-                ["google-chrome", "--remote-debugging-port=9222", "--headless", "--disable-gpu", "--no-sandbox"])
+                ["/usr/bin/google-chrome", "--remote-debugging-port=9222", "--headless", "--disable-gpu",
+                 "--no-sandbox"])
+            time.sleep(5)
             options = webdriver.ChromeOptions()
             options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
             service = Service(executable_path=ChromeDriverManager().install())
