08335f919b223c1658643e9e0ca2cf09e5d0e59b
jasonchung6 <ymca6987@gmail.com>
2023-11-29 18:12:11 +0900

    feat: CodeforecesService 추가 구현
    제곧내
    + 코드포스 카테고리 db 카테고리에 분류 후 적용 필요
    솔브닥과 다르게 한 번에 여러 개가 들어와서 쿼리 바꿔야 함.
    ProblemDao.py에 일단 새 카테고리 insert 쿼리 작성해서 주석처리 해놨습니다.
    
    feat: #31

--- app/service/CodeforcesService.py
+++ app/service/CodeforcesService.py
@@ -11,15 +11,16 @@ from app.dao.ProblemDao import ProblemDao
 from app.model.CodeforcesProblem import CodeforcesProblem
 from app.util.ChromeDriver import ChromeDriver
 from app.util.DatabaseConnection import DatabaseConnection
-from app.util.ErrorLogger import errorLog
+from app.util.ErrorLogger import ErrorLogger
 
-service = "codeforces"
+slackAlert = 0  # (dev)터미널 print 0 / (main)Slackbot 사용 시 1
+logger = ErrorLogger("Codeforces", slackAlert)
 
 
 def crawlCodeforces():
     driver = ChromeDriver()
     wait = WebDriverWait(driver, 10)
-    print("dev/ Codeforces 크롤링이 시작되었습니다.\n")
+    logger.log("Codeforces 크롤링이 시작되었습니다.\n")
     crawlPages(driver, wait)
 
 
@@ -38,6 +39,7 @@ def crawlPages(driver, wait):
             DatabaseConnection.startTransaction()
 
         time.sleep(random.uniform(8, 12))
+    logger.log(f"Codeforces 한 바퀴 돌았십니더...")
 
 
 def getPageNumber(driver, wait):
@@ -51,16 +53,16 @@ def openPage(driver, link):
     try:
         driver.get(link)
     except Exception as e:
-        print(f"페이지 열기 실패: {link} / Exception: {e}\n")
-        errorLog(service, link, "NULL", e)
+        logger.log(f"페이지 열기 실패: {link} / Exception: {e}\n")
+        logger.logError(link, e)
 
 
 def openProblemSetPage(driver, page):
     try:
         driver.get("https://codeforces.com/problemset/page/" + str(page))
     except Exception as e:
-        print(f"페이지 열기 실패: page= {page} / Exception: {e}\n")
-        errorLog(service, page, 0, e)
+        logger.log(f"페이지 열기 실패: page= {page} / Exception: {e}\n")
+        logger.logError(page, e)
 
 
 def getProblemData(driver, wait, page):
@@ -69,14 +71,12 @@ def getProblemData(driver, wait, page):
         rows = driver.find_elements(By.XPATH, '//tbody/tr')
         rows.pop(0)
     except Exception as e:
-        # SlackBot.alert(cId, page, "페이지 크롤링 실패\n Exception: ", e)
-        print(f"{page}페이지 크롤링 실패 / Exception: {e}\n")
-        errorLog(service, page, 0, e)
+        logger.log(f"{page}페이지 크롤링 실패 / Exception: {e}\n")
+        logger.logError(page, e)
         return
 
     for row in rows:
         try:
-            print(row.text)
             td1 = row.find_element(By.XPATH, './/td[1]/a')
             code = td1.text.strip()
             url = td1.get_attribute('href')
@@ -93,5 +93,6 @@ def getProblemData(driver, wait, page):
                                         categoryId=0, solvedCount=solvedCount, realDifficulty=difficulty, )
             ProblemDao.save(problem)
         except Exception as e:
-            errorLog(service, page, 0, e)
+            logger.log(f"{page}페이지 문제 데이터 처리 중 오류 / Exception: {e}\n")
+            logger.logError(page, e)
             continue
