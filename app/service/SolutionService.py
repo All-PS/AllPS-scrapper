8382d50ffe1be3240ce8211a1c3b18a3589c8f72
jasonchung6 <124449788+jasonchung6@users.noreply.github.com>
2024-01-06 18:12:24 +0900

    refactor: New Codeforce Solution Crawler
    코드포스 크롤러 방식 변경
    todo: Kotlin 등 특정 언어 전용 콘테스트 대응

--- app/service/SolutionService.py
+++ app/service/SolutionService.py
@@ -6,12 +6,13 @@ from app.entity.Solution import Solution
 
 
 class SolutionService():
-    def __init__(self, notification, progLangName):
+    def __init__(self, notification, progLangName, platformName):
         databaseConfig = DatabaseConfig()
         self.dbConnection = databaseConfig.getConnection()
         self.solutionDao = SolutionDao(self.dbConnection)
         self.notification = notification
         self.progLangName = progLangName
+        self.platformName = platformName
 
     def saveSolution(self, solutionDto):
         programmingLanguage = ProgrammingLanguage(self.progLangName)
@@ -28,3 +29,17 @@ class SolutionService():
             self.notification.alert(f"[Error] 데이터베이스에 솔루션을 저장하는데 오류가 발생했습니다. ()")
             self.dbConnection.rollback()
             raise e
+
+    def findProblems(self):
+        try:
+            # 트랜잭션 필요?????
+            # self.dbConnection.begin()
+            problemCodes = self.solutionDao.get(self.platformName)
+            return problemCodes
+            # self.dbConnection.commit()
+        except Exception as e:
+            self.notification.alert(f"[Error] DB 솔루션 없는 문제 찾기 오류 발생 ()")
+            # self.dbConnection.rollback()
+            raise e
+            return None
+
